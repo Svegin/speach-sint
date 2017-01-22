85da9500c15ec4ee72ca6f334e25f4954eba2f10
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-22 15:18:19 -0600

    add voices

--- master.js
+++ master.js
@@ -1,6 +1,20 @@
 const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
+// console.log(voicesDropdown);
 const options = document.querySelectorAll('[type="range"], [name="text"]');
+// console.log(options);
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+  .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+  .join('');
+}
+
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
